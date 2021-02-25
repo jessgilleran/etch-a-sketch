bebbfd9d13e79f5ae162b5deff7235ea527a3fc6
Jessica Gilleran <jess.gilleran@gmail.com>
2021-02-24 19:31:35 -0500

    added rainbow and shading options

--- script.js
+++ script.js
@@ -1,32 +1,86 @@
-//on page open, generate a grid of 16x16 divs
-//When mouse passes over div, they change colour
-//Button clears grid and asks user for size
+//Initial page setup
 
-//Create grid on page open
+//Get container element
 const container = document.querySelector('.grid-container');
+
+//Initial grid settings
 let gridSize = 16;
-const baseColorLabel = document.querySelector('#base-color');
-createGrid(gridSize);
+let bgColor = 'rgb(199,117,235)';
+let shading = false;
+let rainbowColor = false;
+
+//Initialize page controls
+//Clean grid button
+const cleanButton = document.querySelector('.clean-button');
+cleanButton.addEventListener('click',cleanGrid);
+
+//Grid size input
 const gridSizeInput = document.querySelector('#grid-size');
 gridSizeInput.value = gridSize;
 gridSizeInput.addEventListener('change',validateInput);
 
+//Get the color picker element and create event
+const colorPicker = document.querySelector('#color-picker');
+colorPicker.addEventListener('change',setbgColor);
 
+//Create the color label
+const currentColorLabel = document.querySelector('#current-color');
+currentColorLabel.innerText = bgColor;
 
+const rainbowCheckbox = document.querySelector('#rainbow-checkbox');
+rainbowCheckbox.addEventListener('change',toggleRainbow);
+
+const shadingCheckbox = document.querySelector('#shading-checkbox');
+shadingCheckbox.addEventListener('change',toggleShading);
+
+//Create the starting grid
+createGrid(gridSize);
+
+function createGrid (size) {
+    //Format the grid
+    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    container.style.gridColumnGap = '1px';
+    container.style.gridRowGap = '1px';
+    
+    for (i=0;i<(size**2);i++) {
+        const div1 = document.createElement('div');
+        div1.classList.add('square');
+        div1.style['background-color'] = 'white';
+        container.appendChild(div1);
+    }
+
+    const gridDivs = document.querySelectorAll('.square');
+    createGridEvent(gridDivs);
+}
+
+function setbgColor(event) {
+    bgColor = event.target.value;
+    currentColorLabel.innerText = bgColor;
+    //clearGrid();
+    //createGrid(gridSize);
+}
 
 function createGridEvent (divList) {
-    let bgColor = getRandomColor();
-    baseColorLabel.innerText = bgColor;
+    
     let shadeFactor = getShadeFactor(bgColor);
 
     let bgColorValues = separateColor(bgColor);
     for (const item of divList) {
         item.addEventListener("mouseenter", function( event ) {
-            // highlight the mouseenter target
-            //If the background color is white, change to the chosen bg color
-            if (item.style.backgroundColor === 'white') {
-                event.target.setAttribute('style', `background: ${bgColor}`);
-            } 
+            
+            //If the background color is white, change to the chosen bg color 
+            //or rainbow if rainbow is true
+            if (item.style.backgroundColor === 'white' && rainbowColor === false) {
+                item.setAttribute('style', `background: ${bgColor}`);
+                //Set the base-color attribute to the current color
+                item.setAttribute('data-base-color',bgColor);
+                console.log(item.getAttribute('data-base-color'));
+            } else if (item.style.backgroundColor === 'white' && rainbowColor === true) {
+                item.setAttribute('style', `background: ${getRandomColor()}`);
+                item.setAttribute('data-base-color',item.style.backgroundColor);
+                console.log(item.getAttribute('data-base-color'));
+            }
             //If the background color is not white, take the current 
             //background color and minus the shade factor
             else {
@@ -39,6 +93,30 @@ function createGridEvent (divList) {
     }
 }
 
+function toggleRainbow (event) {
+    //Check if input is checked
+    if (event.target.checked === false) {
+        rainbowColor = false;
+        currentColorLabel.innerText = bgColor;
+    } else {
+        rainbowColor = true;
+        currentColorLabel.innerText = 'Rainbow!';
+    }
+}
+
+function toggleShading (event) {
+    //Check if input is checked
+    if (event.target.checked === false) {
+        shading = false;
+    } else {
+        shading = true;
+    }
+}
+
+
+
+
+
 function getShadeFactor(rgbValue) {
     
     let colorArray = separateColor(rgbValue);
@@ -46,7 +124,7 @@ function getShadeFactor(rgbValue) {
     let rFactor = colorArray[0]/10;
     let gFactor = colorArray[1]/10;
     let bFactor = colorArray[2]/10;
-
+    console.log([rFactor,gFactor,bFactor]);
     return [rFactor,gFactor,bFactor];
 }
 
@@ -80,19 +158,17 @@ function validateInput(e) {
         gridSize = e.target.value;
         gridSizeInput.value = gridSize;
         clearGrid();
-        createGrid(e.target.value);
+        createGrid(gridSize);
     }
 }
 
 function cleanGrid() {
     for (const item of (document.querySelectorAll('.square'))) {
-        item.style.backgroundColor = 'white'
-        item.style.border = '1px solid #5e6363';
+        item.style.backgroundColor = 'white';
     }
 }
 
-const cleanButton = document.querySelector('.clean-button');
-cleanButton.addEventListener('click',cleanGrid);
+
 
 function clearGrid() {
     document.querySelectorAll('.square').forEach(el => el.remove());
@@ -101,25 +177,6 @@ function clearGrid() {
     
 }
 
-function createGrid (size) {
 
-    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-    
-    container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
-    
-    for (i=0;i<(size**2);i++) {
-        const div1 = document.createElement('div');
-        div1.classList.add('square');
-        div1.style['border'] = '1px solid #5e6363'
-        div1.style['background-color'] = 'white';
-        
-        container.appendChild(div1);
-    }
-
-    const gridDivs = document.querySelectorAll('.square');
-    createGridEvent(gridDivs);
-    //container.style.gridTemplateColumns = 'repeat(size,1fr)';
-    
-}
 
 
